162bc82a547f6543801eafa44614d0221ae8f3b8
Leonard Mulyarchik <leo.mulyarchik@gmail.com>
2022-10-05 21:07:19 +0300

    init: add react router & prettier config

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,9 +1,15 @@
-export function Product(){
-    return(
-        <div><h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Product!</h3>
-        <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-            The Zero Gravity Pen can be used to write in any orientation,
-            including upside-down. It even works in outer space.
-        </p></div>
-    )
+import React from "react";
+
+export function Product() {
+  return (
+    <div>
+      <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
+        Product!
+      </h3>
+      <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+        The Zero Gravity Pen can be used to write in any orientation, including
+        upside-down. It even works in outer space.
+      </p>
+    </div>
+  );
 }
--- src/components/search-panel/search-panel.tsx
+++ src/components/search-panel/search-panel.tsx
@@ -1,6 +1,5 @@
+import React from "react";
 import { useState } from "react";
-
-import "./App.css";
 
 function App() {
   const [] = useState("");
